722fb665d4a66a1e391db10b2707c7398db78ce3
Luca Virgilio <luca.virgilio95@gmail.com>
2019-10-23 18:41:16 +0200

    create Angular app

---
+++ app_public/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeListComponent } from './home-list/home-list.component';
+
+@NgModule({
+  declarations: [
+    HomeListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [HomeListComponent]
+})
+export class AppModule { }
